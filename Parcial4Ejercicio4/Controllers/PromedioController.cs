51f50bbc84ebb59f66c9f9677978c7f22941244b
ArmandoOrellana <73487533+ArmandoOrellana@users.noreply.github.com>
2020-11-20 20:57:29 -0600

    Agregar archivos de proyecto.

---
+++ Parcial4Ejercicio4/Controllers/PromedioController.cs
@@ -0,0 +1,41 @@
+﻿using Parcial4Ejercicio4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Parcial4Ejercicio4.Controllers
+{
+    public class PromedioController : Controller
+    {
+        // GET: Promedio
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Promedio()
+        {
+            Datos objModelo = new Datos();
+            objModelo.codigo = Request.Form["codigo"].ToString();
+            objModelo.nAlumno = Request.Form["nEstudiante"].ToString();
+            objModelo.nMateria = Request.Form["nMateria"].ToString();
+
+            objModelo.valor1 = double.Parse(Request.Form["valor1"].ToString());
+            objModelo.valor2 = double.Parse(Request.Form["valor2"].ToString());
+            objModelo.valor3 = double.Parse(Request.Form["valor3"].ToString());
+
+            objModelo.promedio = (objModelo.valor1 + objModelo.valor2 + objModelo.valor3)/3;
+            if (objModelo.promedio <= 4.0)
+            {
+                objModelo.aprobacion = "Reprobado";
+            }
+            else
+            {
+                objModelo.aprobacion = "Aprobado";
+            }
+
+            return View("Promedio",objModelo);
+        }
+    }
+}
---
+++ Parcial4Ejercicio4/Models/Datos.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Parcial4Ejercicio4.Models
+{
+    public class Datos
+    {
+        public string codigo { get; set; }
+        public string nAlumno { get; set; }
+        public string nMateria { get; set; }
+
+
+        public double valor1 { get; set; }
+        public double valor2 { get; set; }
+        public double valor3 { get; set; }
+        public double promedio { get; set; }
+
+        public string aprobacion { get; set; }
+    }
+}
